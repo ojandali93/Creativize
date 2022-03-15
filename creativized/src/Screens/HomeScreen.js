de2a676c0f3dcd1219fffd3ba881977b55982c6b
omar jandali <omarjandali93@gmail.com>
2022-03-15 12:28:12 -0700

    Empty app started and running on iPhone

---
+++ creativized/src/Screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { View, Text, Button, StyleSheet } from 'react-native'
+
+export default function HomeScreen() {
+  return (
+    <View>
+        <Text> Home Screen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+})
