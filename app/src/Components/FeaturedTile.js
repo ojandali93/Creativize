af4e939f40340c7ccab24ff889987245a91c0f01
omar jandali <omarjandali93@gmail.com>
2022-03-16 15:34:30 -0700

    setup the basic home page that renders based on mock data

---
+++ app/src/Components/FeaturedTile.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import { StyleSheet, View, Text, Image } from 'react-native'
+
+export default function FeaturedTile(props) {
+  const film = props.film
+
+  return (
+    <View style={styles.tileContent}>
+      <Image style={styles.thumbnail} source={require("../../assets/images/sf-1-min.png")}/>
+      <View style={styles.tileDetails}>
+        <View style={styles.rowData}>
+          <View style={styles.titleName}>
+            <Image style={styles.badge} source={require('../../assets/outline_workspace_premium_black_24dp.png')}/>
+            <Text style={styles.title}>{film.name}</Text>
+          </View>
+          <Text style={styles.genre}>{film.genre}</Text>  
+        </View>
+        <View style={styles.rowData}>
+          <Text style={styles.text}>{film.director}</Text>
+          <Text style={styles.text}>{film.release}</Text>
+          <Text style={styles.text}>{film.length}</Text>
+        </View>
+        <View style={styles.rowData}>
+          <Text style={styles.text}>{film.blurb}</Text>
+        </View>
+      </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  tileContent: {
+    display: 'flex',
+    flexDirection: 'row'
+  },
+  thumbnail: {
+    height: 260,
+    width: 160
+  },
+  tileDetails: {
+    padding: 4
+  },
+  rowData: {
+    width: 235,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingBottom: 4
+  },
+  titleName: {
+    flexDirection: 'row',
+    alignItems: 'center'
+  },
+  badge: {
+    height: 20,
+    width: 20
+  },
+  title: {
+    fontSize: 22,
+    fontWeight: 'bold'
+  },
+  genre: {
+    fontSize: 20
+  },
+  text: {
+    fontSize: 16
+  }
+})
--- app/src/Screens/HomeScreen.js
+++ app/src/Screens/HomeScreen.js
@@ -1,40 +1,98 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import ShoftFilmTile from "../Components/ShoftFilmTile"
+import MockData from '../../assets/MockData.json'
+import { FlatList, ScrollView } from 'react-native-gesture-handler'
 
 export default function HomeScreen() {
+
+  const [shorts, setShorts] = useState(MockData)
+
   return (
     <View style={styles.app}>
-      <View style={styles.section}>
-        <Text style={styles.category}>Award Winning</Text>
-        <View style={styles.scrollBoxHorizontal}>
-          <Text>Hello World</Text>
-          <Text>Hello World</Text>
-          <Text>Hello World</Text>
+      <ScrollView>
+        <View style={styles.section}>
+          <Text style={styles.category}>Award Winning</Text>
+          <View style={styles.scrollBoxHorizontal}>
+            <FlatList
+              horizontal
+              keyExtractor={film => film.id}
+              data={shorts}
+              renderItem={(film) => {
+                return(<ShoftFilmTile film={film} badge="award-winning"/>)
+              }}
+            />
+          </View>
         </View>
-      </View>
+        <View style={styles.section}>
+          <Text style={styles.category}>Recent Releases</Text>
+          <View style={styles.scrollBoxHorizontal}>
+            <FlatList
+              style={styles.featuredFilms}
+              horizontal
+              keyExtractor={film => film.id}
+              data={shorts}
+              renderItem={(film) => {
+                return(<ShoftFilmTile film={film} badge="new-release"/>)
+              }}
+            />
+          </View>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.category}>Staff Picks</Text>
+          <View style={styles.scrollBoxHorizontal}>
+            <FlatList
+              horizontal
+              keyExtractor={film => film.id}
+              data={shorts}
+              renderItem={(film) => {
+                return(<ShoftFilmTile film={film} badge="staff-pick"/>)
+              }}
+            />
+          </View>
+        </View>
+        <View style={styles.section}>
+          <Text style={styles.category}>Featured</Text>
+          <View style={{backgroundColor: 'lightgrey'}}>
+            <FlatList
+              keyExtractor={film => film.id}
+              data={shorts}
+              renderItem={(film) => {
+                return(<ShoftFilmTile film={film} badge="featured"/>)
+              }}
+            />
+          </View>
+        </View>
+      </ScrollView>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   app: {
+    display: 'flex',
     backgroundColor: 'white',
-    height: '100%'
+    overflow: 'hidden',
+    width: '100%',
+    margin : 0,
+    padding: 0
   },
   section: {
-    backgroundColor: 'beige',
-    padding: 16
+    padding: 12
   },
   category: {
     fontSize: 24,
     fontWeight: 'bold'
   },
   scrollBoxHorizontal: {
-    display: 'flex',
     flexDirection: 'row',
     overflow: 'hidden',
     overflow: 'scroll',
-    justifyContent: 'center'
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    maxHeight: 550
+  },
+  featuredFilms: {
+    width: '100%',
   }
 })
